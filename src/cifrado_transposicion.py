59d8ef5bd959775cbdc44a2beac573401edd5d09
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-18 20:13:06 -0500

    Funcion encryptMessage
    Nombre de la funcion a encriptar mensaje

--- src/cifrado_transposicion.py
+++ src/cifrado_transposicion.py
@@ -24,3 +24,28 @@ def main():
     print(ciphertext + '|')
 
 
+def encryptMessage(key, message):
+    # Each string in ciphertext represents a column in the grid.
+    ciphertext = [''] * key
+    print(ciphertext)
+    input()
+
+    # Loop through each column in ciphertext.
+    for column in range(key):
+        currentIndex = column
+
+        # Keep looping until currentIndex goes past the message length.
+        while currentIndex < len(message):
+            # Place the character at currentIndex in message at the
+            # end of the current column in the ciphertext list.
+            ciphertext[column] += message[currentIndex]
+
+            # move currentIndex over
+            currentIndex += key
+
+    # Convert the ciphertext list into a single string value and return it.
+    return ''.join(ciphertext)
+
+
+if __name__ == '__main__':
+    main()
