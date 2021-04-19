9d27ee1813df9aee193f25144eefd666e90d56f8
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-18 20:12:07 -0500

    Funcion main
    Nombre de la funcion principal

--- src/cifrado_transposicion.py
+++ src/cifrado_transposicion.py
@@ -6,3 +6,21 @@ Python: 3.9.1
 Fecha: 07 Abril 2020
 """
 
+def main():
+    myMessage = input('Ingresa el mensaje a cifrar: ')
+    espacios = 1
+    while espacios > 0:
+        clave = input('Ingresa tu palabra clave para cifrar: ')
+        espacios = clave.count(' ')
+        if clave.isalpha() == False:
+            espacios += 1
+    myKey = len(clave)
+
+    ciphertext = encryptMessage(myKey, myMessage)
+
+    # Print the encrypted string in ciphertext to the screen, with
+    # a | ("pipe" character) after it in case there are spaces at
+    # the end of the encrypted message.
+    print(ciphertext + '|')
+
+
